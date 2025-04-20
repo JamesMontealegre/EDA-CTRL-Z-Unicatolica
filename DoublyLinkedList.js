e713ae80fbef25b849126286e6e63e67a2db927b
MapacheLoC0 <jhonatanmordorig@gmail.com>
2025-04-19 20:06:26 -0500

    MapacheLoC0/Implemente el metodo InsertSortByFirstName

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -9,6 +9,44 @@ class DoublyLinkedList {
     return this.size === 0; // Ver si la lista está vacía
   }
 
-  //Metodos aqui
+  insertSortedByFirstName(newNode) {
+    if (!(newNode instanceof DoublyLinkedListNode)) {
+      throw new Error("Solo se pueden meter nodos de tipo DoublyLinkedListNode.");
+    }
+
+    const newNameLower = newNode.firstName.toLowerCase();
+
+    if (!this.head) {
+      // Si no hay nada, este nodo es el primero
+      this.head = newNode;
+      this.tail = newNode;
+    } else if (newNameLower < this.head.firstName.toLowerCase()) {
+      // Si el nuevo nodo va antes del primero
+      newNode.next = this.head;
+      this.head.previous = newNode;
+      this.head = newNode;
+    } else {n
+      // Si toca meterlo en el medio o al final
+      let currentNode = this.head;
+
+      while (currentNode.next !== null && currentNode.next.firstName.toLowerCase() < newNameLower) {
+        currentNode = currentNode.next;
+      }
+
+      if (currentNode.next === null) {
+        // Si va al final
+        currentNode.next = newNode;
+        newNode.previous = currentNode;
+        this.tail = newNode;
+      } else {
+        // Si va en el medio
+        newNode.next = currentNode.next;
+        newNode.previous = currentNode;
+        currentNode.next.previous = newNode;
+        currentNode.next = newNode;
+      }
+    }
+    this.size++; // Aumentar el tamaño
+  }
 
 }
